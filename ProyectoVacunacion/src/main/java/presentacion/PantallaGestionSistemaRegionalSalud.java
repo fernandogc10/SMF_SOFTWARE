485150f029b963362d111a753346a5211645c9a0
SergioDiazTech <sergiodp0604@gmail.com>
2021-10-28 18:11:58 +0200

    Creacion PantallaGestionSistemaRegionalSalud

--- ProyectoVacunacion/src/main/java/presentacion/PantallaGestionSistemaRegionalSalud.java
+++ ProyectoVacunacion/src/main/java/presentacion/PantallaGestionSistemaRegionalSalud.java
@@ -1,4 +1,58 @@
+
 package presentacion;
 
-public class PantallaGestionSistemaRegionalSalud {
+import java.io.*;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+
+public class PantallaGestionSistemaRegionalSalud extends JFrame {
+
+	private JPanel contentPane;
+	
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PantallaConsultaEstadisticas frame = new PantallaConsultaEstadisticas();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PantallaGestionSistemaRegionalSalud() {
+		setResizable(false);
+		setTitle("Sistema Regional de Salud");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(".\\src\\main\\resources\\LogoAPP.png"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 795, 473);
+		contentPane = new JPanel();
+		contentPane.setForeground(new Color(64, 224, 208));
+		contentPane.setBackground(new Color(240,255,255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
 }
