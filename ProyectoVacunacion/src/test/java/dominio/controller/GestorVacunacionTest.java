ed8b2554c51c4c28ee35b7a4ab6b1058d6f08225
fernandogc10 <fernando.guerrero.cano@gmail.com>
2021-11-17 12:53:21 +0100

    update files with Junit

--- ProyectoVacunacion/src/test/java/dominio/controller/GestorVacunacionTest.java
+++ ProyectoVacunacion/src/test/java/dominio/controller/GestorVacunacionTest.java
@@ -1,18 +1,18 @@
 package dominio.controller;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class GestorVacunacionTest {
 
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
 	}
 
-	@After
+	@AfterEach
 	public void tearDown() throws Exception {
 	}
 
