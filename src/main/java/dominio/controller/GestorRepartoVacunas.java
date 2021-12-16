6ab9a66284b2ae8bc42b87c6d6ac8e65d83b7eb7
fernandogc10 <fernando.guerrero.cano@gmail.com>
2021-12-16 11:05:16 +0100

    update bug sonarcloud

--- src/main/java/dominio/controller/GestorRepartoVacunas.java
+++ src/main/java/dominio/controller/GestorRepartoVacunas.java
@@ -1,17 +1,11 @@
 package dominio.controller;
 
-import java.sql.SQLException;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Vector;
 
 import dominio.entitymodel.EntregaVacunas;
 import dominio.entitymodel.LoteVacunas;
-import persistencia.AgenteBD;
 
 public class GestorRepartoVacunas {
 
@@ -23,7 +17,7 @@ public class GestorRepartoVacunas {
 
 	}
 
-	public static List<EntregaVacunas> calcularEntregasRegion() throws SQLException, Exception { // devuelve una lista
+	public static List<EntregaVacunas> calcularEntregasRegion() throws Exception { // devuelve una lista
 																									// donde se deberian
 																									// de entregar
 
