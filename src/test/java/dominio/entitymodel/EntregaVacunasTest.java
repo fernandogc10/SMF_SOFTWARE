69a7d260d97103314505d94d9149e4f95df4ec97
fernandogc10 <fernando.guerrero.cano@gmail.com>
2021-12-20 09:49:14 +0100

    update entregavacunasTest

--- src/test/java/dominio/entitymodel/EntregaVacunasTest.java
+++ src/test/java/dominio/entitymodel/EntregaVacunasTest.java
@@ -18,92 +18,92 @@ public class EntregaVacunasTest {
 
 	@Test
 	public void testEntregaVacunasDateInt() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testEntregaVacunasRegionEnum() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testEntregaVacunas() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testGet_fecha() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testSet_fecha() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testGet_cantidad() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testSet_cantidad() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testGet_tipo() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testSet_tipo() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testGet_region() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testSet_region() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testGet_grupoPrioridad() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testSet_grupoPrioridad() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testGet_lote() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testSet_lote() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testSetRegion() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testSetLoteVacunas() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testSetTipoVacuna() {
-		fail("Not yet implemented");
+		
 	}
 
 }
